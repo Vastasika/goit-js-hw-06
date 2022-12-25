7665fd3aa43d92ac83df8c3c3998c7fb14e90a16
Anastasiia Vakarchuk <111005415+Vastasika@users.noreply.github.com>
2022-12-25 17:03:54 +0200

    first

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заполните все поля, пожалуйста!");
+  }
+
+  console.log(`Email: ${email.value} Password: ${password.value}`);
+
+  e.currentTarget.reset();
+}
